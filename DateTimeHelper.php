b144bf5fffa69c1a32dc0c724841a2108ef39f16
Fabian Peter Hammerle <fabian.hammerle@gmail.com>
2016-08-03 09:41:28 +0800

    added \DateTimeHelper::parse()

--- DateTimeHelper.php
+++ DateTimeHelper.php
@@ -20,4 +20,42 @@ class DateTimeHelper
             throw new \InvalidArgumentException('expected integer or null');
         }
     }
+
+    /**
+     * @param string|null $text
+     * @return DatePeriod|null
+     */
+    public static function parse($text)
+    {
+        if($text === null) {
+            return null;
+        } else {
+            if(preg_match(
+                    '/^(?P<y>\d{4})-(?P<m>\d{2})-(?P<d>\d{2})'
+                        .'([ T](?P<h>\d{2}):(?P<i>\d{2}):(?P<s>\d{2}))?$/',
+                    $text,
+                    $attr
+                    )) {
+                $start = new \DateTime();
+                $start->setDate($attr['y'], $attr['m'], $attr['d']);
+                $start->setTime(
+                    isset($attr['h']) ? $attr['h'] : 0,
+                    isset($attr['i']) ? $attr['i'] : 0,
+                    isset($attr['s']) ? $attr['s'] : 0
+                    );
+                if(isset($attr['h'])) {
+                    $interval = new \DateInterval('PT1S');
+                } else {
+                    $interval = new \DateInterval('P1D');
+                }
+                $end = clone $start;
+                $end->add($interval);
+                return new \DatePeriod($start, $interval, $end);
+            } else {
+                throw new \InvalidArgumentException(
+                    sprintf("could not parse string '%s'", $text)
+                    );
+            }
+        }
+    }
 }
--- tests/DateTimeHelperTest.php
+++ tests/DateTimeHelperTest.php
@@ -6,67 +6,119 @@ use fphammerle\helpers\DateTimeHelper;
 
 class DateTimeHelperTest extends \PHPUnit_Framework_TestCase
 {
-    public function testTimestampToDateTimeZero()
+    public function timestampToDateTimeProvider()
     {
-        $this->assertEquals(
-            0,
-            DateTimeHelper::timestampToDateTime(0)->getTimestamp()
-            );
-    }
-
-    public function testTimestampToDateTimeCompareUTC()
-    {
-        $created = DateTimeHelper::timestampToDateTime(1234567890);
-        $expected = new \DateTime('2009-02-13 23:31:30', new \DateTimeZone('UTC'));
-        $this->assertEquals($created->getTimestamp(), $expected->getTimestamp());
-    }
-
-    public function testTimestampToDateTimeCompareLocal()
-    {
-        $created = DateTimeHelper::timestampToDateTime(1234567890);
-        $expected = new \DateTime('2009-02-14 00:31:30', new \DateTimeZone('Europe/Vienna'));
-        $this->assertEquals($created->getTimestamp(), $expected->getTimestamp());
-    }
-
-    public function testTimestampToDateTimeSetLocal()
-    {
-        date_default_timezone_set('Europe/Vienna');
-        $this->assertEquals(
-            DateTimeHelper::timestampToDateTime(123456)->getTimestamp(),
-            123456
-            );
-    }
-
-    public function testTimestampToDateTimeSetUTC()
-    {
-        date_default_timezone_set('UTC');
-        $this->assertEquals(
-            DateTimeHelper::timestampToDateTime(123456)->getTimestamp(),
-            123456
-            );
-    }
-
-    public function testTimestampToDateTimeNull()
-    {
-        $this->assertEquals(
-            DateTimeHelper::timestampToDateTime(null),
-            null
-            );
+        return [
+            [null, null],
+            [0, new \DateTime('1970-01-01 00:00:00', new \DateTimeZone('UTC'))],
+            [0, new \DateTime('1970-01-01 01:00:00', new \DateTimeZone('Europe/Vienna'))],
+            [1234567890, new \DateTime('2009-02-13 23:31:30', new \DateTimeZone('UTC'))],
+            [1234567890, new \DateTime('2009-02-14 00:31:30', new \DateTimeZone('Europe/Vienna'))],
+            [-3600, new \DateTime('1970-01-01 00:00:00', new \DateTimeZone('Europe/Vienna'))],
+            ];
     }
 
     /**
-     * @expectedException InvalidArgumentException
+     * @dataProvider timestampToDateTimeProvider
      */
-    public function testTimestampToDateInvalidArgumentFloat()
+    public function testTimestampToDateTime($timestamp, $expected_datetime)
     {
-        DateTimeHelper::timestampToDateTime(1.23);
+        $generated_datetime = DateTimeHelper::timestampToDateTime($timestamp);
+        $this->assertEquals($expected_datetime, $generated_datetime);
+    }
+
+    public function timestampToDateTimeDefaultTimezoneProvider()
+    {
+        return [
+            ['UTC', 100],
+            ['Europe/Vienna', 0],
+            ['Europe/Vienna', -100],
+            ['Europe/Vienna', 100],
+            ['Europe/London', 3600],
+            ['US/Pacific', 3600],
+            ];
     }
 
     /**
-     * @expectedException InvalidArgumentException
+     * @dataProvider timestampToDateTimeDefaultTimezoneProvider
      */
-    public function testTimestampToDateInvalidArgumentString()
+    public function testTimestampToDateTimeDefaultTimezone($timezone, $timestamp)
     {
-        DateTimeHelper::timestampToDateTime('');
+        date_default_timezone_set($timezone);
+        $generated_datetime = DateTimeHelper::timestampToDateTime($timestamp);
+        $this->assertSame($timestamp, $generated_datetime->getTimestamp());
+    }
+
+    public function parseProvider()
+    {
+        return [
+            [null, 'UTC', null],
+            [null, 'US/Pacific', null],
+            ['2016-08-02', 'UTC', new \DatePeriod(
+                new \DateTime('2016-08-02T00:00:00Z'),
+                new \DateInterval('P1D'),
+                new \DateTime('2016-08-03T00:00:00Z')
+                )],
+            ['2016-08-02', 'Europe/Vienna', new \DatePeriod(
+                new \DateTime('2016-08-02T00:00:00+02:00'),
+                new \DateInterval('P1D'),
+                new \DateTime('2016-08-03T00:00:00+02:00')
+                )],
+            ['2016-08-02', 'Europe/Vienna', new \DatePeriod(
+                new \DateTime('2016-08-01T22:00:00Z'),
+                new \DateInterval('P1D'),
+                new \DateTime('2016-08-02T22:00:00Z')
+                )],
+            ['2016-08-02 15:52:13', 'UTC', new \DatePeriod(
+                 new \DateTime('2016-08-02T15:52:13Z'),
+                 new \DateInterval('PT1S'),
+                 new \DateTime('2016-08-02T15:52:14Z')
+                 )],
+            ['2016-08-02 15:52:13', 'Europe/Vienna', new \DatePeriod(
+                 new \DateTime('2016-08-02T15:52:13+02:00'),
+                 new \DateInterval('PT1S'),
+                 new \DateTime('2016-08-02T15:52:14+02:00')
+                 )],
+            ['2016-08-02 15:52:13', 'Europe/Vienna', new \DatePeriod(
+                 new \DateTime('2016-08-02T13:52:13Z'),
+                 new \DateInterval('PT1S'),
+                 new \DateTime('2016-08-02T13:52:14Z')
+                 )],
+            ['2016-08-02T15:52:13', 'US/Pacific', new \DatePeriod(
+                 new \DateTime('2016-08-02T15:52:13-07:00'),
+                 new \DateInterval('PT1S'),
+                 new \DateTime('2016-08-02T15:52:14-07:00')
+                 )],
+            ];
+    }
+
+    /**
+     * @dataProvider parseProvider
+     */
+    public function testParse($text, $timezone, $expected)
+    {
+        date_default_timezone_set($timezone);
+        $this->assertEquals($expected, DateTimeHelper::parse($text));
+    }
+
+    public function parseInvalidArgumentProvider()
+    {
+        return [
+            ['     '],
+            [''],
+            ['2016--12'],
+            ['2016-10-12 08:20#01'],
+            [1],
+            [false],
+            ];
+    }
+
+    /**
+     * @dataProvider parseInvalidArgumentProvider
+     * @expectedException \InvalidArgumentException
+     */
+    public function testParseInvalidArgument($text)
+    {
+        DateTimeHelper::parse($text);
     }
 }
